6671905bfc9e6ca307ca7893e953e75bfc78c9b0
Ernest Guevarra <ernest@guevarra.io>
2025-07-03 06:08:38 +0100

    complete review report

---
+++ R/onedrive.R
@@ -0,0 +1,37 @@
+#'
+#' Find required file/s
+#' 
+#' @param onedrive
+#' @param src
+#' @param dest
+#' @param overwrite
+#' 
+
+onedrive_download_item <- function(onedrive, src, dest, overwrite = FALSE) {
+  ## Look for file in personal files ----
+  pfiles <- onedrive$list_files()
+
+  if (src %in% pfiles$name) {
+    onedrive$download_file(src, dest = dest, overwrite = overwrite)
+  } else {
+    item_list <- onedrive$list_shared_files()
+    
+    onedrive_item <- lapply(
+      X = item_list, 
+      FUN = function(x) {
+        x$get_path() |>
+          grepl(pattern = src, x = _)
+      }
+    ) |>
+      unlist() |>
+      (\(x) item_list[x])() |>
+      (\(x) x[[1]])()
+
+    onedrive_item$download(dest = dest, overwrite = overwrite)
+  }
+  
+  dest
+}
+
+
+
--- _targets.R
+++ _targets.R
@@ -6,6 +6,15 @@ suppressPackageStartupMessages(source("packages.R"))
 for (f in list.files(here::here("R"), full.names = TRUE)) source (f)
 
 
+## Microsoft OneDrive interface setup ----
+onedrive_targets <- tar_plan(
+  tar_target(
+    name = onedrive,
+    command = get_business_onedrive()
+  )
+)
+
+
 ## Data targets ----
 data_targets <- tar_plan(
   tar_target(
@@ -15,35 +24,54 @@ data_targets <- tar_plan(
   tar_target(
     name = sids_islands,
     command = sids_list$island
+  ),
+  tar_target(
+    name = extraction_matrix_data_file,
+    command = onedrive_download_item(
+      onedrive = onedrive, src = "Extraction_matrix_1.xlsx",
+      dest = "data-raw/extraction_matrix.xlsx", overwrite = TRUE
+    ),
+    format = "file"
+  ),
+  tar_target(
+    name = extraction_matrix_data,
+    command = readxl::read_xlsx(
+      path = extraction_matrix_data_file,
+      range = "A1:AE24"
+    )
   )
 )
 
 
-### Processing targets
+## Processing targets
 processing_targets <- tar_plan(
   
 )
 
 
-### Analysis targets
+## Analysis targets
 analysis_targets <- tar_plan(
   
 )
 
 
-### Output targets
+## Output targets
 output_targets <- tar_plan(
   
 )
 
 
-### Reporting targets
+## Reporting targets
 report_targets <- tar_plan(
-  
+  tar_quarto(
+    name = data_review_report,
+    path = "reports/sc-ca-qoc-data-review.qmd",
+    working_directory = here::here()
+  )
 )
 
 
-### Deploy targets
+## Deploy targets
 deploy_targets <- tar_plan(
   
 )
