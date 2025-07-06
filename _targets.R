949f2e0ffa596f578e527398a5587db9280dc33f
Ernest Guevarra <ernest@guevarra.io>
2025-07-06 22:06:21 +0100

    edit targets for onedrive

--- _targets.R
+++ _targets.R
@@ -6,15 +6,6 @@ suppressPackageStartupMessages(source("packages.R"))
 for (f in list.files(here::here("R"), full.names = TRUE)) source (f)
 
 
-## Microsoft OneDrive interface setup ----
-onedrive_targets <- tar_plan(
-  tar_target(
-    name = onedrive,
-    command = get_business_onedrive()
-  )
-)
-
-
 ## Data targets ----
 data_targets <- tar_plan(
   tar_target(
@@ -28,7 +19,7 @@ data_targets <- tar_plan(
   tar_target(
     name = extraction_matrix_data_file,
     command = onedrive_download_item(
-      onedrive = onedrive, src = "Extraction_matrix_1.xlsx",
+      src = "Extraction_matrix_1.xlsx", 
       dest = "data-raw/extraction_matrix.xlsx", overwrite = TRUE
     ),
     format = "file"
