7e3bd5594e585e9a94f2b0db5aa66cc8cfb6f21a
Ernest Guevarra <ernest@guevarra.io>
2025-07-06 21:29:24 +0100

    add heuristic for checking extraction file

--- R/onedrive.R
+++ R/onedrive.R
@@ -8,28 +8,31 @@
 #' 
 
 onedrive_download_item <- function(onedrive, src, dest, overwrite = FALSE) {
-  ## Look for file in personal files ----
-  pfiles <- onedrive$list_files()
+  ## Check if dest exists ----
+  if (!file.exists(dest)) {
+    ## Look for file in personal files ----
+    pfiles <- onedrive$list_files()
 
-  if (src %in% pfiles$name) {
-    onedrive$download_file(src, dest = dest, overwrite = overwrite)
-  } else {
-    item_list <- onedrive$list_shared_files()
+    if (src %in% pfiles$name) {
+      onedrive$download_file(src, dest = dest, overwrite = overwrite)
+    } else {
+      item_list <- onedrive$list_shared_files()
     
-    onedrive_item <- lapply(
-      X = item_list, 
-      FUN = function(x) {
-        x$get_path() |>
-          grepl(pattern = src, x = _)
-      }
-    ) |>
-      unlist() |>
-      (\(x) item_list[x])() |>
-      (\(x) x[[1]])()
+      onedrive_item <- lapply(
+        X = item_list, 
+        FUN = function(x) {
+          x$get_path() |>
+            grepl(pattern = src, x = _)
+        }
+      ) |>
+        unlist() |>
+        (\(x) item_list[x])() |>
+        (\(x) x[[1]])()
 
-    onedrive_item$download(dest = dest, overwrite = overwrite)
+      onedrive_item$download(dest = dest, overwrite = overwrite)
+    }
   }
-  
+
   dest
 }
 
